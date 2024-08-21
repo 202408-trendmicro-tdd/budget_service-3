8414d01765736248ed40b252f15983e665041b54
joeychen <joeychen@odd-e.com>
2024-08-21 22:14:57 +0800

    clean up and remove useless(empty) if/else block

--- src/main/java/org/example/Period.java
+++ src/main/java/org/example/Period.java
@@ -9,16 +9,6 @@ public record Period(LocalDate startDate, LocalDate endDate) {
     long getOverlappingDays(Budget budget) {
         LocalDate overlappingStart = startDate.isAfter(budget.firstDay()) ? startDate : budget.firstDay();
         LocalDate overlappingEnd = endDate.isBefore(budget.lastDay()) ? endDate : budget.lastDay();
-        if (budget.getYearMonth().equals(YearMonth.from(startDate()))) {
-//            overlappingStart = startDate();
-//            overlappingEnd = budget.lastDay();
-        } else if (budget.getYearMonth().equals(YearMonth.from(endDate()))) {
-//            overlappingStart = budget.firstDay();
-//            overlappingEnd = endDate();
-        } else if (budget.getYearMonth().isAfter(YearMonth.from(startDate())) && budget.getYearMonth().isBefore(YearMonth.from(endDate()))) {
-//            overlappingStart = budget.firstDay();
-//            overlappingEnd = budget.lastDay();
-        }
         return DAYS.between(overlappingStart, overlappingEnd) + 1;
     }
 }
