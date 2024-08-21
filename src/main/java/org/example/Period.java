053fd3282af79563350f5906b692f778d53f2553
joeychen <joeychen@odd-e.com>
2024-08-21 22:14:32 +0800

    use period overlapping domain logic to decide overlappingEnd

--- src/main/java/org/example/Period.java
+++ src/main/java/org/example/Period.java
@@ -8,16 +8,16 @@ import static java.time.temporal.ChronoUnit.DAYS;
 public record Period(LocalDate startDate, LocalDate endDate) {
     long getOverlappingDays(Budget budget) {
         LocalDate overlappingStart = startDate.isAfter(budget.firstDay()) ? startDate : budget.firstDay();
-        LocalDate overlappingEnd = null;
+        LocalDate overlappingEnd = endDate.isBefore(budget.lastDay()) ? endDate : budget.lastDay();
         if (budget.getYearMonth().equals(YearMonth.from(startDate()))) {
 //            overlappingStart = startDate();
-            overlappingEnd = budget.lastDay();
+//            overlappingEnd = budget.lastDay();
         } else if (budget.getYearMonth().equals(YearMonth.from(endDate()))) {
 //            overlappingStart = budget.firstDay();
-            overlappingEnd = endDate();
+//            overlappingEnd = endDate();
         } else if (budget.getYearMonth().isAfter(YearMonth.from(startDate())) && budget.getYearMonth().isBefore(YearMonth.from(endDate()))) {
 //            overlappingStart = budget.firstDay();
-            overlappingEnd = budget.lastDay();
+//            overlappingEnd = budget.lastDay();
         }
         return DAYS.between(overlappingStart, overlappingEnd) + 1;
     }
