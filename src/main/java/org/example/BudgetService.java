e5a947609ef5ffcc0f32e63314db8cd47346a0a9
joeychen <joeychen@odd-e.com>
2024-08-21 22:11:55 +0800

    extract method: getOverlappingDays()

--- src/main/java/org/example/BudgetService.java
+++ src/main/java/org/example/BudgetService.java
@@ -16,6 +16,23 @@ public class BudgetService {
         this.BudgetRepo = BudgetRepo;
     }
 
+    private static long getOverlappingDays(LocalDate startDate, LocalDate endDate, Budget budget) {
+        LocalDate overlappingStart = null;
+        LocalDate overlappingEnd = null;
+        if (budget.getYearMonth().equals(YearMonth.from(startDate))) {
+            overlappingStart = startDate;
+            overlappingEnd = budget.lastDay();
+        } else if (budget.getYearMonth().equals(YearMonth.from(endDate))) {
+            overlappingStart = budget.firstDay();
+            overlappingEnd = endDate;
+        } else if (budget.getYearMonth().isAfter(YearMonth.from(startDate)) && budget.getYearMonth().isBefore(YearMonth.from(endDate))) {
+            overlappingStart = budget.firstDay();
+            overlappingEnd = budget.lastDay();
+        }
+        long overlappingDays = DAYS.between(overlappingStart, overlappingEnd) + 1;
+        return overlappingDays;
+    }
+
     public double totalAmount(LocalDate startDate, LocalDate endDate) {
         if (startDate.isAfter(endDate)) {
             return 0;
@@ -41,19 +58,7 @@ public class BudgetService {
 
         double totalAmount = 0;
         for (Budget budget : budgets) {
-            LocalDate overlappingStart = null;
-            LocalDate overlappingEnd = null;
-            if (budget.getYearMonth().equals(YearMonth.from(startDate))) {
-                overlappingStart = startDate;
-                overlappingEnd = budget.lastDay();
-            } else if (budget.getYearMonth().equals(YearMonth.from(endDate))) {
-                overlappingStart = budget.firstDay();
-                overlappingEnd = endDate;
-            } else if (budget.getYearMonth().isAfter(YearMonth.from(startDate)) && budget.getYearMonth().isBefore(YearMonth.from(endDate))) {
-                overlappingStart = budget.firstDay();
-                overlappingEnd = budget.lastDay();
-            }
-            long overlappingDays = DAYS.between(overlappingStart, overlappingEnd) + 1;
+            long overlappingDays = getOverlappingDays(startDate, endDate, budget);
             totalAmount += budget.dailyAmount() * overlappingDays;
         }
 
